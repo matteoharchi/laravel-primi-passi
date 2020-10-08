6100aebb3a0e595ea0db6c7f043acc4bef67d308
Matteo Harchi <matteo.harchi@gmail.com>
2020-10-08 22:44:11 +0200

    fatta pagina products

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,25 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{asset ('css/app.css')}}">
-    <title>Document</title>
-</head>
-<body>
-    <header>
-        <img src="https://www.lamolisana.it/wp-content/uploads/2020/04/marchio-sito-test.png" alt="">
-        <nav>
-            <ul>
-                <li> <a href="{{route('home')}}">HOME</a> </li>
-                <li><a href="{{route('products')}}">PRODOTTI</a></li>
-                <li><a href="{{route('contacts')}}">CONTATTI</a></li>
-            </ul>
-        </nav>
-    </header>
+@include ('partials.header')
 
     <main>
-        
+
 
     </main>
 </body>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{asset ('css/app.css')}}">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <img src="https://www.lamolisana.it/wp-content/uploads/2020/04/marchio-sito-test.png" alt="">
+        <nav>
+            <ul>
+                <li> <a href="{{route('home')}}">HOME</a></li>
+                <li><a href="{{route('products')}}">PRODOTTI</a></li>
+                <li><a href="{{route('contacts')}}">CONTATTI</a></li>
+            </ul>
+        </nav>
+    </header>
