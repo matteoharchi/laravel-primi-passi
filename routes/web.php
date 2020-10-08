6042557bfd2f0f4f378164dbc1c8dea65a7167e3
Matteo Harchi <matteo.harchi@gmail.com>
2020-10-08 16:13:32 +0200

    Problema encode

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('home');
+
+Route::get('/products', function () {
+    return view('products');
+})->name('products');
+
+Route::get('/contacts', function () {
+    return view('contacts');
+})->name('contacts');
